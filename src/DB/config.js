078cc7e8f8c03a51049497bce97160133ee32ae5
xananax <xananax@yelostudio.com>
2020-08-08 22:07:15 +0300

    Weird Firebase thingy forces me to change IDs

--- src/DB/config.js
+++ src/DB/config.js
@@ -5,8 +5,8 @@ const firebaseConfig = {
   projectId: "volunteer-lebanon",
   storageBucket: "volunteer-lebanon.appspot.com",
   messagingSenderId: "412013759321",
-  appId: "1:412013759321:web:5d45920a8380f0d4bc9a48",
-  measurementId: "G-BHVMMTPC3H"
+  appId: "1:412013759321:web:c242ffabc35d9c48bc9a48",
+  measurementId: "G-TQ1EJ2RVHS"
 };
 
 export default firebaseConfig
